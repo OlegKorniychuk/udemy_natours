87a2ac96bc667bc98d1e82679881d9a6e3f6a323
OlegKorniychuk <kornoleg576@gmail.com>
2024-08-03 19:53:49 +0300

    feat: add .env file with some variables

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
-
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 
 app.use('/api/v1/tours', tourRouter);
