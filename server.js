b966c375629629b4948e27d4431f382111c996da
OlegKorniychuk <kornoleg576@gmail.com>
2024-08-04 17:27:06 +0300

    feat: add mongoDB connection

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 /* eslint-disable no-console */
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './.env' });
+
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  'PASSWORD',
+  process.env.DATABASE_PASSWORD,
+);
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log('DB connection successful');
+  })
+  .catch((err) => {
+    console.log('Could not connect to the database');
+    console.log(err.message);
+  });
 
 const port = process.env.PORT || 3000;
 
