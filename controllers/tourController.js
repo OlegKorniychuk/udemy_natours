fde5842de854affdce3940764556b1c117208556
OlegKorniychuk <kornoleg576@gmail.com>
2024-08-06 20:16:28 +0300

    feat: add pagination for tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -50,6 +50,19 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = limit * (page - 1);
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) {
+        throw new Error('This page does not exist');
+      }
+    }
+
     const tours = await query;
     res.status(200).json({
       status: 'success',
