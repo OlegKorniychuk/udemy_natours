10ddc18c6b5a587939e9159e4ea6059268b7a66f
OlegKorniychuk <kornoleg576@gmail.com>
2024-08-12 19:50:53 +0300

    feat: add separate error handlers for dev and prod

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,40 @@
-module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+/* eslint-disable no-console */
+const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
+    error: err,
+    stack: err.stack,
   });
+};
+
+const sendErrorProd = (err, res) => {
+  // Expected operational errors
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    // Unexpected programming errors
+  } else {
+    console.log('ERROR', err);
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong!',
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  const env = process.env.NODE_ENV;
+  if (env === 'development') {
+    sendErrorDev(err, res);
+  } else if (env === 'production') {
+    sendErrorProd(err, res);
+  }
+
   next();
 };
