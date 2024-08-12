e3b4f9d709547d67ff70d37baf07be94396bf07b
OlegKorniychuk <kornoleg576@gmail.com>
2024-08-12 22:05:36 +0300

    feat: add uncaught exceptions handling

--- server.js
+++ server.js
@@ -2,6 +2,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('App crashed - uncaught exception!');
+  console.log(err.name, ' - ', err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './.env' });
 
 const app = require('./app');
@@ -27,7 +33,7 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhadledRejection', (err) => {
-  console.log(err.name, err.message);
+  console.log(err.name, ' - ', err.message);
   console.log('App crashed - unhandled rejection!');
   server.close(() => {
     process.exit(1);
