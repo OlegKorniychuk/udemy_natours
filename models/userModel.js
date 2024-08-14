5cc6d1691c90df22f30e3b1fbf4878dbffed49a1
OlegKorniychuk <kornoleg576@gmail.com>
2024-08-14 21:21:37 +0300

    fix: fix user model creation

--- models/userModel.js
+++ models/userModel.js
@@ -36,6 +36,6 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = new mongoose.Model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
